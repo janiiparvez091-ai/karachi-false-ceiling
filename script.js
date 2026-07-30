1d070d3a4219e52c68c542ebf63b2ab5dcfdea74
janiiparvez091-ai <janiiparvez091@gmail.com>
2026-07-30 13:36:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ document.querySelectorAll(".faq button").forEach(button=>{
         }
     });
 });
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+let answer = btn.nextElementSibling;
+
+document.querySelectorAll(".faq-answer").forEach(item => {
+if(item !== answer){
+item.style.display = "none";
+}
+});
+
+if(answer.style.display === "block"){
+answer.style.display = "none";
+}
+else{
+answer.style.display = "block";
+}
+
+});
+
+});
