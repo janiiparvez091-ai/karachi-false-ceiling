a903b26476fbde0b95c95cc3c0dcb37947152847
janiiparvez091-ai <janiiparvez091@gmail.com>
2026-07-30 10:25:32 +0500

    Add FAQ toggle functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Karachi False Ceiling Website Loaded");
+document.querySelectorAll(".faq button").forEach(button=>{
+    button.addEventListener("click",()=>{
+        let answer = button.parentElement.nextElementSibling;
+
+        if(answer.style.display==="block"){
+            answer.style.display="none";
+            button.innerHTML="+";
+        }else{
+            answer.style.display="block";
+            button.innerHTML="-";
+        }
+    });
+});
